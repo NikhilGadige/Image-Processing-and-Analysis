59e05f852d4221dc4d86722ef62fe102fbee7e3c
G NIKHIL <153369809+NikhilGadige@users.noreply.github.com>
2026-01-28 21:52:05 +0530

    Revert "Create negative_image.m"
    This reverts commit a5bdac8e982003372529d504dd64e432fffba360.

--- Lab_03/negative_image.m
+++
@@ -1,9 +0,0 @@
-
-img = imread(['DIP3E_Original_Images_CH03/Fig0304(a)(breast_digital_Xray).tif']);
-if size(img,3) == 3
-    img = rgb2gray(img);
-end
-neg = 255 - img;
-figure;
-subplot(1,2,1), imshow(img), title('Original Image');
-subplot(1,2,2), imshow(neg), title('Negative Image');
